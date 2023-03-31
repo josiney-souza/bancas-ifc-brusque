e5e40aa41bcaa4e1438796dd46ab2a45242a3bd4
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-03-31 12:08:59 -0300

    Adicionados Views e códigos da tela 4 experimental (titulação do substituto)
    Adicionados Views e códigos da tela 4 (titulação do professor substituto) no APP IFC Brusque v1.0a

---
+++ app/src/main/java/com/example/bancasifcbrusque/SubsTitulosActivity.java
@@ -0,0 +1,14 @@
+package com.example.bancasifcbrusque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubsTitulosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subs_titulos);
+    }
+}
