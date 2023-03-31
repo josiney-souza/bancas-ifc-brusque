a1ec096379e19fbcba19d9763bcf6fbf26f067e7
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-03-31 11:51:41 -0300

    Adicionados Views e códigos da tela inicial
    Adicionados Views e códigos da tela inicial no APP IFC Brusque v1.0a

--- app/src/main/java/com/example/bancasifcbrusque/MainActivity.java
+++ app/src/main/java/com/example/bancasifcbrusque/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.bancasifcbrusque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickTcc (View v) {
+        Intent mudarTela = new Intent();
+        mudarTela.setClass(this, TccActivity.class);
+        startActivity(mudarTela);
+    }
+
+    public void onClickSubsDid (View v) {
+        Intent mudarTela = new Intent();
+        mudarTela.setClass(this, SubsDidaticaActivity.class);
+        startActivity(mudarTela);
+    }
+
+    public void onClickSubsTit (View v) {
+        String mensagem = "Função ainda não implementada";
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickApagarPrefs (View v) {
+        SharedPreferences.Editor editorPrefs = getSharedPreferences("prefsSeekBar", MODE_PRIVATE).edit();
+        editorPrefs.clear();
+        editorPrefs.apply();
+
+        SharedPreferences.Editor editorSubsDid = getSharedPreferences("prefsSubsDid", MODE_PRIVATE).edit();
+        editorSubsDid.clear();
+        editorSubsDid.apply();
+
+        Toast.makeText(this, "As notas salvas da banca de TCC foram apagadas", Toast.LENGTH_SHORT).show();
     }
 }
